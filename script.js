2bf9edf365664a8571cac1b179b1709fc88dc3f3
bolhasss <128045911+bolhasss@users.noreply.github.com>
2023-09-28 08:41:26 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -32,3 +32,28 @@ function image03() {
     image2.classList.add('hide')
     image3.classList.remove('hide')
 }
+
+const cursor = document.querySelector(".cursor");
+    var timeout;
+
+    //follow cursor on mousemove
+    document.addEventListener("mousemove", (e) => {
+      let x = e.pageX;
+      let y = e.pageY;
+
+      cursor.style.top = y + "px";
+      cursor.style.left = x + "px";
+      cursor.style.display = "block";
+
+      //cursor effects when mouse stopped
+      function mouseStopped(){
+        cursor.style.display = "none";
+      }
+      clearTimeout(timeout);
+      timeout = setTimeout(mouseStopped, 8000);
+    });
+
+    //cursor effects when mouseout
+    document.addEventListener("mouseout", () => {
+      cursor.style.display = "none";
+    });
